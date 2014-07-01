9725ac5ee8fc6e5e5d9a67b2b4e7629aa217766d
astankus28 <abriani@abrianis-imac.neo.rr.com>
2014-07-01 15:26:46 -0400

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Penguin.h
@@ -0,0 +1,13 @@
+//
+//  Penguin.h
+//  PeevedPenguins
+//
+//  Created by Abriani on 7/1/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Penguin : CCSprite
+
+@end
